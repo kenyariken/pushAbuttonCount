d5a7b31531a1720c66e8edd8444aac07c5d822f4
Ken <161325958+kenyariken@users.noreply.github.com>
2026-01-07 00:40:10 +0900

    Change counter increment from 3 to 4

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 // Aボタンを押したら数字が1増えるプログラム
 let counter = 0
 input.onButtonPressed(Button.A, function () {
-    counter += 3 // カウンターを３増やす
+    counter += 4 // カウンターを４増やす
     basic.showNumber(counter)　// 今の数字を表示
 })
