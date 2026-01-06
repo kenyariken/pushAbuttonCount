c440ada6ed61046aa6823c5dcfb66c776d30cb5a
Ken <161325958+kenyariken@users.noreply.github.com>
2026-01-07 00:17:11 +0900

    カウンターを2増やす

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 // Aボタンを押したら数字が1増えるプログラム
 let counter = 0
 input.onButtonPressed(Button.A, function () {
-    counter += 1　// カウンターを1増やす
+    counter += 2　// カウンターを2増やす
     basic.showNumber(counter)　// 今の数字を表示
 })
