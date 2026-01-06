b1a26dcc3aa8ed3e140c39c4298f050013a98159
Ken <161325958+kenyariken@users.noreply.github.com>
2026-01-06 23:53:12 +0900

    説明を追加
    Fix formatting issues and add comments for clarity.

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
+// Aボタンを押したら数字が1増えるプログラム
 let counter = 0
 input.onButtonPressed(Button.A, function () {
-    counter += 1
-    basic.showNumber(counter)
+    counter += 1　// カウンターを1増やす
+    basic.showNumber(counter)　// 今の数字を表示
 })
