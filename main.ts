887f16cf421aa50bed3f75aecc1d56b9b1bb245b
Ken <161325958+kenyariken@users.noreply.github.com>
2026-01-07 01:17:55 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
-// Aボタンを押したら数字が1増えるプログラム
 let counter = 0
+/**
+ * Aボタンを押したら数字が1増えるプログラム
+ */
 input.onButtonPressed(Button.A, function () {
-    counter += 6 // カウンターを６増やす
-    basic.showNumber(counter)　// 今の数字を表示
+    // カウンターを６増やす
+    counter += 7
+    // 今の数字を表示
+    basic.showNumber(counter)
 })
