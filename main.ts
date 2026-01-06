68168b76cdf597fb7e9cb7c50311896f31d2729f
Ken <161325958+kenyariken@users.noreply.github.com>
2026-01-07 00:50:20 +0900

    Change counter increment from 5 to 6

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 // Aボタンを押したら数字が1増えるプログラム
 let counter = 0
 input.onButtonPressed(Button.A, function () {
-    counter += 5 // カウンターを５増やす
+    counter += 6 // カウンターを６増やす
     basic.showNumber(counter)　// 今の数字を表示
 })
